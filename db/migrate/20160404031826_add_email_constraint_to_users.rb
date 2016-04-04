e22f4917bab761de18e655e0c67b321b655790f4
tobyfee <tobyfee@gmail.com>
2016-04-04 03:32:35 +0000

    adding requirement that email be a gmail domain, just because I've always wanted to do that with Postgres. In a real situation this would look like a requirement that all meails be within the companyname.com account

---
+++ db/migrate/20160404031826_add_email_constraint_to_users.rb
@@ -0,0 +1,17 @@
+class AddEmailConstraintToUsers < ActiveRecord::Migration
+  def up
+  execute %{ ALTER TABLE
+          users
+        ADD CONSTRAINT
+          email_must_be_company_email
+        CHECK ( email ~* '^[^@]+@gmail\\.com' )
+  }
+  end
+  def down
+  execute %{ ALTER TABLE
+          users
+        DROP CONSTRAINT
+          email_must_be_company_email
+      }
+  end
+end
